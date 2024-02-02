29d2e8dd939e002c3d28b4a77f1a9b263b39ecf0
theo <theo.middleton@outlook.com>
2024-02-02 10:01:56 +0000

    🗃️ Add database and image data schema

---
+++ src/server/db/schema.ts
@@ -0,0 +1,30 @@
+import { relations, sql } from "drizzle-orm";
+import {
+  bigint,
+  index,
+  int,
+  mysqlTableCreator,
+  primaryKey,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
+
+export const mysqlTable = mysqlTableCreator((name) => `portfolio-project_${name}`);
+
+export const imageData = mysqlTable(
+  "imageData",
+  {
+    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+    uuid: varchar("uuid", { length: 36 }).notNull(),
+    fileName: varchar("fileName", { length: 256 }).notNull(),
+    fileUrl: varchar("fileUrl", { length: 256 }).notNull(),
+    uploadedAt: timestamp("uploadedAt")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    modifiedAt: timestamp("modifiedAt").onUpdateNow(),
+  },
+  (example) => ({
+    uuidIndex: index("uuid_idx").on(example.uuid),
+  })
+);
