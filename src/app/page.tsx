e712d9b870e9e61c9f43178181393e1bff74ad65
theo <59509832+theomiddleton@users.noreply.github.com>
2024-03-08 13:51:36 +0000

    further improve photo pages, update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,12 @@ export default async function Home() {
     id: imageData.id,
     fileUrl: imageData.fileUrl,
   }).from(imageData)
-  const imageUrls = result.map((item) => item.fileUrl) 
 
+  const imageUrls = result.map((item) => ({
+    id: item.id,
+    url: item.fileUrl
+  }))
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white text-black">
       <SiteHeader />
@@ -22,10 +26,10 @@ export default async function Home() {
           {siteConfig.headers.main}
         </h1> {/*The following tailwind class is the responsive columns*/}
         <section className="sm:columns-1 md:columns-2 lg:columns-3 xl:columns-4 max-h-5xl mx-auto space-y-4">
-          {imageUrls.map((url) => (
-            <div key={url} className="rounded-md overflow-hidden hover:scale-[0.97] duration-100">
-              <a href={url} target="_blank" rel="noreferrer">
-                <Image src={url} alt="img" height={600} width={400} />
+          {imageUrls.map((image) => (
+            <div key={image.id} className="rounded-md overflow-hidden hover:scale-[0.97] duration-100">
+              <a href={'/photo/' + image.id} target="_self" rel="noreferrer">
+                <Image src={image.url} alt="img" height={600} width={400} />
               </a>
             </div>
           ))}
--- src/app/photo/[id]/page.tsx
+++ src/app/photo/[id]/page.tsx
@@ -1,30 +1,59 @@
+import { sql, eq } from 'drizzle-orm'
+import { SiteHeader } from '~/components/site-header'
 import { db } from '~/server/db'
 import { imageData } from '~/server/db/schema'
+import { siteConfig } from '~/config/site'
+import Image from 'next/image'
 
-export async function generateStaticParams() {
-    const result = await db.select({
-        id: imageData.id,
-        fileUrl: imageData.fileUrl,
-    }).from(imageData)
-    console.log(result)
+export default async function Photo({ params }: { params: { id: number } }) {
 
-    if (!Array.isArray(result)) {
-        console.error('Result is not an array:', result)
-        return
-    }
+  const result = await db.select({
+      id: imageData.id,
+      fileUrl: imageData.fileUrl,
+      name: imageData.name,
+      description: imageData.description,
+      tags: imageData.tags,
+      uploadedAt: imageData.uploadedAt
+  }).from(imageData).where(eq(imageData.id, params.id))
 
-    const imageMeta = result.reduce((map, item) => {
-        map.set(item.id, item.fileUrl)
-        return map
-    }, new Map())
-    
-    console.log(imageMeta)
+  console.log(result)
+  const imageUrl = result.map((item) => item.fileUrl) 
+  console.log("fileUrl", imageUrl)
 
-    return(
-        <section>
-            {imageMeta.get((id) => (
-                <div>Id: {id}</div>
-            ))}
+  return (
+    <main>
+    <SiteHeader />
+    <main className="flex min-h-screen flex-col items-center bg-white text-black">
+    {/*the class above is for the background colour, screen size, and centering the following*/}
+    {/*below is the grid. with two columns it has the image and then the metadata*/}
+    <div className="min-h-screen grid grid-cols-2">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <section className="max-h-5xl mx-auto space-y-4">
+          <div className="rounded-md shadow-2xl">
+            <Image src={imageUrl[0]} alt="img" height={1000} width={600} />
+          </div>
         </section>
-    )
+      </div>
+        {/*the following class holds the image tile and metadata. its to held to the side of the image in column 2, and has a padding of 8*/}
+        {/* idealy when the screen width is small enough, it would go below the image - todo */}
+        <div className="container flex flex-col pl-8 items-start justify-center">
+          <div className="flex max-w-xs flex-col gap-4">
+            <h3 className="text-2xl font-bold">{result[0].name}</h3>
+            <div className="text-lg">
+              {result[0].description}
+            </div>
+          </div>
+          <div className="flex max-w-xs flex-col gap-4">
+            <h3 className="text-2xl font-bold">Tags</h3>
+            <div className="text-lg">
+              {result[0].tags}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    </main>
+  
+  )
+
 }
