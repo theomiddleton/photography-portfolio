38ad982a74080e6a83c02ba0f124c4b89325123d
theomiddleton <59509832+theomiddleton@users.noreply.github.com>
2024-04-24 20:00:39 +0000

    vercel build errors

--- src/app/api/(blog)/blog-fetch/route.ts
+++ src/app/api/(blog)/blog-fetch/route.ts
@@ -2,19 +2,19 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { db } from '~/server/db'
 import { blogImages } from '~/server/db/schema'
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    try{
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
         const result = await db.select({
             id: blogImages.id,
             fileUrl: blogImages.fileUrl,
             description: blogImages.description,
-        }).from(blogImages) 
+        }).from(blogImages);
 
-        console.log('server side Fetched data:', result)
+        console.log('Server side Fetched data:', result);
         
-        res.status(200).json({ result })
+        res.status(200).json({ result });
     } catch (error) {
-        console.error('Error fetching image URL from the database:', error) 
-        res.status(500).json({ error: 'Error fetching image URL from the database' })
+        console.error('Error fetching image URL from the database:', error);
+        res.status(500).json({ error: 'Error fetching image URL from the database' });
     }
 }
