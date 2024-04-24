8a1ab85369f5e4f393eb634051c0367e59924c91
theomiddleton <59509832+theomiddleton@users.noreply.github.com>
2024-04-24 19:54:11 +0000

    vercel build errors

--- src/app/api/(blog)/blog-fetch/route.ts
+++ src/app/api/(blog)/blog-fetch/route.ts
@@ -1,18 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next' 
-import { NextResponse } from 'next/server' 
 import { db } from '~/server/db'
 import { blogImages } from '~/server/db/schema'
 
-export async function GET(res: NextApiResponse) {
-//export async function GET() {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
     try{
         const result = await db.select({
             id: blogImages.id,
             fileUrl: blogImages.fileUrl,
             description: blogImages.description,
-        }).from(blogImages)     
-        return NextResponse.json({ result })
+        }).from(blogImages) 
+
+        console.log('server side Fetched data:', result)
+        
+        res.status(200).json({ result })
     } catch (error) {
         console.error('Error fetching image URL from the database:', error) 
+        res.status(500).json({ error: 'Error fetching image URL from the database' })
     }
 }
--- src/app/api/fetch/route.ts
+++ src/app/api/fetch/route.ts
@@ -1,17 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next' 
+import { NextResponse } from 'next/server' 
 import { db } from '~/server/db'
 import { imageData } from '~/server/db/schema'
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(res: NextApiResponse) {
+//export async function GET() {
     try{
         const result = await db.select({
             id: imageData.id,
             fileUrl: imageData.fileUrl,
         }).from(imageData) 
         
-        res.status(200).json({ result: result })
+        return NextResponse.json({ result: result })
     } catch (error) {
         console.error('Error fetching image URL from the database:', error) 
-        res.status(500).send('Error fetching image URL from the database') 
+        //res.status(500).send('Error fetching image URL from the database') 
     }
 }
