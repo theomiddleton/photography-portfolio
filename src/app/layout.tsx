73f524d7d5ed43a7394740794748b2b29b0fc509
theomiddleton <59509832+theomiddleton@users.noreply.github.com>
2024-04-26 17:14:36 +0000

    add speed insights and remove temp fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css"
 import { siteConfig } from "~/config/site"
 import { SiteFooter } from "~/components/site-footer" 
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { AxiomWebVitals } from 'next-axiom';
 
 import { Inter } from "next/font/google" 
@@ -28,6 +29,7 @@ export default function RootLayout({
         {children}
         <Analytics />
         <AxiomWebVitals />
+        <SpeedInsights />
       </body>
       <SiteFooter />
     </html>
