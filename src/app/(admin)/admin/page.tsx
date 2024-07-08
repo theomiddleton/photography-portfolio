1d35adb03d5933fff301e3fa5f62f4f204eaa354
theo <59509832+theomiddleton@users.noreply.github.com>
2024-07-08 11:34:47 +0100

    Fix env validation issues

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -8,14 +8,15 @@
 
   import { NotAuthenticated } from '~/components/not-authenticated'
 
-  import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+  // import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
 
   export default function Admin() {
 
-    const { isAuthenticated, isLoading } = useKindeBrowserClient()
-    if (isLoading) return <div>Loading...</div>
+    // const { isAuthenticated, isLoading } = useKindeBrowserClient()
+    // if (isLoading) return <div>Loading...</div>
 
-  return isAuthenticated ? (
+  // return isAuthenticated ? (
+  return (
       <div className="min-h-screen bg-white text-black space-y-12">
         <div className="max-w-2xl mx-auto py-24 px-4">
           <h2 className="text-base font-semibold leading-7 text-black">
@@ -24,7 +25,7 @@
         <UploadImg/>
       </div>
     </div>
-  ) : (
-    <NotAuthenticated />
+  // ) : (
+    // <NotAuthenticated />
   )
 }
--- src/env.js
+++ src/env.js
@@ -8,30 +8,10 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z
-      .string()
-      .url()
-      .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
-        "You forgot to change the default URL"
-      ),
+      .string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
-    ),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
-    GITHUB_CLIENT_ID: z.string(),
-    GITHUB_CLIENT_SECRET: z.string(),
     KINDE_CLIENT_ID: z.string(),
     KINDE_CLIENT_SECRET: z.string(),
     KINDE_ISSUER_URL: z.string(),
@@ -62,12 +42,6 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-    GITHUB_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     NEXT_PUBLIC_R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
     NEXT_PUBLIC_R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
     NEXT_PUBLIC_R2_IMAGE_BUCKET_NAME: process.env.R2_IMAGE_BUCKET_NAME,
