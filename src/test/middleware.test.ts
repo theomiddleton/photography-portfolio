5c752ced165c32407798de81623fa16cde2bbaec
theo <59509832+theomiddleton@users.noreply.github.com>
2024-07-17 14:25:26 +0100

    work on tests

--- src/test/middleware.test.ts
+++ src/test/middleware.test.ts
@@ -9,39 +9,48 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 describe('middleware', () => {
-    let server
-    beforeAll(async () => {
-        await app.prepare()
-        server = createServer((req, res) => {
-        const parsedUrl = parse(req.url, true)
-        handle(req, res, parsedUrl)
-        })
-        await new Promise<void>((resolve, reject) => {
-        server.listen(3000, (err) => {
-            if (err) return reject(err)
-            resolve()
-        })
-        })
-    })
-    
-    afterAll(async () => {
-        await app.close()
-        await new Promise<void>((resolve, reject) => {
-        server.close((err) => {
-            if (err) return reject(err)
-            resolve()
-        })
-        })
-    })
-    
-    it('should redirect to signin if not authenticated', async () => {
-        try {
-            const response = await request(server).get('/admin')
-            expect(response.statusCode).toBe(200)
-            expect(response.header.location).toBe('/signin')
-        } catch (error) {
-            console.error("test error: ", error)
-            throw error
+  let server
+
+  beforeAll(async () => {
+    await app.prepare()
+    server = createServer((req, res) => {
+      const parsedUrl = parse(req.url, true)
+      handle(req, res, parsedUrl)
+    });
+
+    await new Promise<void>((resolve, reject) => {
+      server.listen(3000, (err) => {
+        if (err) {
+          console.error('Server listen error:', err)
+          return reject(err)
         }
+        resolve()
+      })
     })
+  })
+
+  afterAll(async () => {
+    await app.close();
+    await new Promise<void>((resolve, reject) => {
+      server.close((err) => {
+        if (err) {
+          console.error('Server close error:', err)
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+  })
+
+  it('should redirect to signin if not authenticated', async () => {
+    try {
+      const response = await request(server).get('/admin')
+      expect(response.statusCode).toBe(200)
+      expect(response.header.location).toBe('/signin')
+    } catch (error) {
+      console.error('Test error:', error)
+      throw error
+    }
+  })
 })
+
