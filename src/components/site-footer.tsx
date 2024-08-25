21adb97f3185c601d40f8844b8768f8666504d6b
theomiddleton <59509832+theomiddleton@users.noreply.github.com>
2024-08-25 20:49:14 +0000

    fix header and footer sticking on page

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -6,34 +6,34 @@ import Link from "next/link"
 
 export function SiteFooter() {
   return (
-    <footer className="sticky bottom-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <footer className="fixed bottom-0 left-0 z-50 w-full border-t border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <div className="w-full flex-1 md:w-auto md:flex-none">
           </div>
           <nav className="flex items-center">
-              <Link
-                href={siteConfig.links.github}
-                target="_blank"
-                rel="noreferrer"
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={cn(
+                  buttonVariants({
+                    variant: "ghost",
+                  }),
+                  "w-9 px-0"
+                )}
               >
-                <div
-                  className={cn(
-                    buttonVariants({
-                      variant: "ghost",
-                    }),
-                    "w-9 px-0"
-                  )}
-                >
-                  <Icons.gitHub className="h-4 w-4" />
-                  <span className="sr-only">GitHub</span>
-                </div>
-              </Link>
-              <Link
-                href={siteConfig.links.website}
-                target="_blank"
-                rel="noreferrer"
-              >
+                <Icons.gitHub className="h-4 w-4" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.website}
+              target="_blank"
+              rel="noreferrer"
+            >
               <div
                 className={cn(
                   buttonVariants({
@@ -45,7 +45,7 @@ export function SiteFooter() {
                 <Icons.website className="h-3 w-3 fill-current" />
                 <span className="sr-only">website</span>
               </div>
-              </Link>
+            </Link>
           </nav>
         </div>
       </div>
