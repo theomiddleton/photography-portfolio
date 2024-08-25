5805a099badfc64742c170d2d2b3408d370efe13
theomiddleton <59509832+theomiddleton@users.noreply.github.com>
2024-08-25 20:49:31 +0000

    fix

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { siteConfig } from "~/config/site"
 import { cn } from "~/lib/utils"
 import { Icons } from "~/components/ui/icons"
-import { MainNav } from "~/components/main-nav"*
+import { MainNav } from "~/components/main-nav"
 import { buttonVariants } from "~/components/ui/button"
 
 export function SiteHeader() {
