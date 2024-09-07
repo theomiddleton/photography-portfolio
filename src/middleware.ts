9f64efe0610d469c5a3f979c471dc9b2de962e14
Theo Middleton <theo@Theos-iMac.lan>
2024-09-07 14:31:53 +0100

    auth fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,7 @@
-import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware"
-
-export default withAuth(
-  async function middleware(req) {
-    console.log("auth", req.kindeAuth)
-  },
-  {
-    isReturnToCurrentPage: false,
-    loginPage: "/signin",
-    isAuthorized: ({token}) => {
-      return token.permissions.includes("admin:admin");
-    }
-  }
-)
-
+import {withAuth} from '@kinde-oss/kinde-auth-nextjs/middleware'
+export default function middleware(req) {
+  return withAuth(req)
+}
 export const config = {
-  matcher: [
-    "/admin/:path*",
-    // "/empty",
-  ]
+  matcher: ["/admin/:path*"]
 }
