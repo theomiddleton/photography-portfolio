ffc1e7d41edfe796fef26da361a3e0261e1d1eb3
theo <59509832+theomiddleton@users.noreply.github.com>
2024-09-16 13:55:33 +0100

    comment the upload api route

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -9,22 +9,30 @@ import { db } from '~/server/db'
 import { imageData, storeImages } from '~/server/db/schema'
 
 export async function POST(request: Request) {
+  // Get the file name, name, description, tags, and isSale status from the request
   const { filename, name, description, tags, isSale } = await request.json()
   console.log(filename, ',', name, ',', description, ',', tags, ',', isSale)
 
   try {
+    // take the file extention from the filename
     const fileExtension = filename.split('.').pop()
+    // create a unique key name for the image
     const keyName = uuidv4() 
-
+    
+    // create a new PutObjectCommand with the bucket name and key name
     const command = new PutObjectCommand({
       Bucket: process.env.R2_IMAGE_BUCKET_NAME,
       Key: keyName + '.' + fileExtension,
     }) 
+    // get a signed URL for the PutObjectCommand
     const url = await getSignedUrl(r2, command, { expiresIn: 60 }) 
     //console.log('server side url', url)
+    // get the new filename with the key name and file extension
     const newFileName = keyName + '.' + fileExtension
+    // create the file URL with the bucket URL and new file name
     const fileUrl =`${siteConfig.bucketUrl}/${newFileName}`
     console.log(fileUrl)
+    // insert the image data into the database
     await db.insert(imageData).values({
       uuid: keyName, 
       fileName: newFileName, 
@@ -36,6 +44,7 @@ export async function POST(request: Request) {
 
     console.log('isSale status ', isSale)
     
+    // fetch that image data from the database
     const result = await db
       .select({
         id: imageData.id,
@@ -44,7 +53,8 @@ export async function POST(request: Request) {
       .from(imageData)
       .where(eq(imageData.uuid, sql.placeholder('uuid')))
       .execute({ uuid: keyName })
-
+    
+    // insert the store image data into the database
     await db.insert(storeImages).values({
       imageId: result[0].id,
       imageUuid: keyName, 
@@ -55,8 +65,10 @@ export async function POST(request: Request) {
       //change to a componetnt within the image upload to set stock and price
     })
 
+    // return the signedUrl to the client to upload the image
     return Response.json({ url })
   } catch (error) {
+    // otherwise, return an error message
     console.error(error) 
     return Response.json({ error: error.message })
   }
