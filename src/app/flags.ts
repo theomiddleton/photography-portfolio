d7610ffc8777b95446868a60a28ae00be51ed6ef
theo <59509832+theomiddleton@users.noreply.github.com>
2025-01-25 19:38:57 +0000

    remove dead code

--- src/app/flags.ts
+++
@@ -1,8 +0,0 @@
-import { get } from '@vercel/edge-config';
-
-export async function getFlags() {
-  const flags = await get('featureFlags') as Record<string, boolean> | null;
-  return {
-    altImagePage: flags?.['alt-image-page'] ?? false,
-  }
-}
