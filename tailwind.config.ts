066a6a832a01e8fc7156652711a3a6180a1e4b6c
Theo Middleton <59509832+theomiddleton@users.noreply.github.com>
2025-06-28 01:52:05 +0100

    refactor(tailwind): remove unused safelist configuration from tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,14 +82,7 @@ const config: Config = {
     require('@tailwindcss/typography'),
     require('tailwind-scrollbar'),
     require("tailwindcss-animate")
-  ],
-  safelist: [
-    'data-[style=ghost]:bg-transparent',
-    'data-[style=primary]:bg-blue-500',
-    'data-[active-state=on]:bg-gray-200',
-    'data-[state=open]:bg-gray-200',
-    'data-[highlighted=true]:bg-gray-100',
-  ],
+  ]
 }
 
 export default config
