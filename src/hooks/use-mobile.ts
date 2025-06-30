96c603c7785b4df82b09554cb11078ecc7297f66
Theo Middleton <59509832+theomiddleton@users.noreply.github.com>
2025-07-01 00:50:46 +0100

    chore(ui): Update shadcn components

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,8 +1,8 @@
-import * as React from 'react'
+import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
-export function useMobile() {
+export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
@@ -10,9 +10,9 @@ export function useMobile() {
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-    mql.addEventListener('change', onChange)
+    mql.addEventListener("change", onChange)
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener('change', onChange)
+    return () => mql.removeEventListener("change", onChange)
   }, [])
 
   return !!isMobile
