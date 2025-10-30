05ffaade09e4d811d70afdcc3ccfc694eaeec488
theomiddleton <59509832+theomiddleton@users.noreply.github.com>
2025-10-30 18:01:38 +0000

    Fix eslint config, upgrade to eslint 9 flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,19 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
 import stylistic from '@stylistic/eslint-plugin'
-import prettier from 'eslint-config-prettier'
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
   {
     plugins: { '@stylistic': stylistic },
     files: ['**/*.{js,jsx,ts,tsx}'],
@@ -34,8 +26,6 @@ const eslintConfig = [
       '@stylistic/jsx-quotes': ['warn', 'prefer-single'],
     },
   },
-  // --- Prettier compatibility (turns off conflicting stylistic rules) ---
-  prettier,
-]
+])
 
 export default eslintConfig
